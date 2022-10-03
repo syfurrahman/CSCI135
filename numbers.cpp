52809e9d894aaddf0d172196bddce4333ff403e6
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2022-10-03 18:24:04 -0400

    numbers2.cpp
    A prime number is an integer greater or equal to 2 that is only divisible by 1 and by itself. The first few primes are: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 …
    
    N is a prime if and only if it is not divisible evenly by any of the numbers from 2 to N−1. Let’s implement this decision as a function.
    
    In the same program numbers.cpp, add a function
    
    bool isPrime(int n);
    The function should return true if n is a prime, otherwise return false. Change the main function to test your new code.

--- numbers.cpp
+++ numbers.cpp
@@ -10,17 +10,27 @@
 Author: Syfur Rahman
 Course: CSCI-135
 Professor: Genady Maryash
-Assignment:  Lab 5 Task A
-Write a program numbers.cpp that defines a function 
-bool isDivisibleBy(int n, int d);
-If n is divisible by d, the function should return true, otherwise return false.
+Assignment:  Lab 5 Task A,B,C,D,E,F,G
+N is a prime if and only if it is not divisible evenly by any of the numbers from 2 to N−1. 
+Let’s implement this decision as a function.
 */
 
 #include <iostream>
   
 using namespace std;
 
-
+//bool isPrime(int n);
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            return false;
+        } 
+    } 
+    return true;
+}
 
 //bool isDivisibleBy(int n, int d);
 bool isDivisibleBy(int n, int d) {
@@ -43,10 +53,10 @@ int n, d;
 
 cout << "Enter n: " << endl; 
 cin >> n;
-cout << "Enter d: " << endl; 
-cin >> d;
+//cout << "Enter d: " << endl; 
+//cin >> d;
 
-cout << isDivisibleBy(n, d) << endl;
+cout << isPrime( n)  << endl;
 
 }
 
